e00c676f0a756b37d59dbc7ac97babb3a185753d
NicholasConti <conti94@live.it>
2023-04-04 15:11:41 +0200

    add html

--- badword.php
+++ badword.php
@@ -1,6 +1,13 @@
-<?php
-
-echo $_GET['frase'];
-echo strlen($_GET['frase']);
-   
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Word</title>
+</head>
+<body>
+    <div> La frase è: <?php echo $_GET['frase'] ?> </div>
+    <div> La frase è lunga: <?php echo strlen($_GET['frase']) ?> caratteri</div>
+</body>
+</html>
